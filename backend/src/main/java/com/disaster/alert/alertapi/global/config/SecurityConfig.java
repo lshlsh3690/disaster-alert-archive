b31b3ec2025caf2f777ab4c5b0680c6f9e285ab1
lshlsh3690 <lshlsh3690@gmail.com>
2025-10-22 02:57:05 +0900

    security config 수정

--- backend/src/main/java/com/disaster/alert/alertapi/global/config/SecurityConfig.java
+++ backend/src/main/java/com/disaster/alert/alertapi/global/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://disaster-alert-archive.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
